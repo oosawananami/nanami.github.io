74d5b7ba56b604a70930d1d534336ba364c3ac52
nanami <h_daniel_min@yahoo.co.jp>
2022-07-05 23:27:57 +0900

    アスペクト比など修正

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+
+$(function(){
+    $('.picture').fadeIn(3000);
+
+});
+
+
+// ハンバーガーメニュー
 $('.burger').on('click',function(){
   $('.burger').toggleClass('close');
   $('.header ul').fadeToggle(500);
