2d9044b5fbb9764e0f6f929f14075f5e5bf155f8
nanami <h_daniel_min@yahoo.co.jp>
2022-07-14 15:48:46 +0900

    スライド修正

--- script.js
+++ script.js
@@ -18,10 +18,12 @@ $(function(){
     $('.slide').slick({
       mobileFirst:true,
       autoplay:true,//自動スライド
+      autospeed:500,
       infinite:true,//スライドループ
       slidesToShow:1,
       slidesToScroll:1,
       centerMode:true,
+      variablewidth:true,
       arrows: false, //previousのボタン非表示
       responsive:[{
         breakpoint:500, //画面サイズが500以上の場合はSlick表示じゃなくする
